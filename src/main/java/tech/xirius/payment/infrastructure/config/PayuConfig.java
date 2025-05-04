9ee248630dc116083d8e1f4490ac7c03734e2872
Harold Velasquez <hvelasquez@unicauca.edu.co>
2025-05-03 22:59:52 -0500

    PayuConfig

---
+++ src/main/java/tech/xirius/payment/infrastructure/config/PayuConfig.java
@@ -0,0 +1,52 @@
+package tech.xirius.payment.infrastructure.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "payu")
+public class PayuConfig {
+
+    private Account account;
+    private Merchant merchant;
+    private Api api;
+    private Transaction transaction;
+
+    @Getter
+    @Setter
+    public static class Account {
+        private String id;
+    }
+
+    @Getter
+    @Setter
+    public static class Merchant {
+        private String id;
+    }
+
+    @Getter
+    @Setter
+    public static class Api {
+        private String login;
+        private String key;
+        private String url;
+
+    }
+
+    @Getter
+    @Setter
+    public static class Transaction {
+        private String language;
+        private String command;
+        private String type;
+        private String paymentCountry;
+        private boolean test;
+
+    }
+
+}
